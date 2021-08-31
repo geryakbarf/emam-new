6e1ccf43365ca67d7478f113cf330039926bacba
Chiqo Rifky Saputra <bandi_nada@live.com>
2021-08-31 21:38:38 +0700

    - Tambahan aksi accept dan reject utk admin melakukan verifikasi ownership - WIP untuk Update additional information in approval (fitur ini masih disembunyikan) (file yang terkait: form_approval.js, edit-approvals.html, approval_form_nav.html, general.html, owner.html)

--- handlers/v1/approvals.js
+++ handlers/v1/approvals.js
@@ -22,21 +22,100 @@ const getRequestedPlaces = async (req, res) => {
     }
 }
 
-const acceptRequest = async (req, res) => {
+const getOneRequestedPlace = async (req, res) => {
+    try {
+        const data = await Place.findOne({_id: req.params.id});
+        return res.json({message: "Success to retrive places", data})
+    } catch (error) {
+        console.log(error);
+        if (error.code)
+            return res.status(error.code).json(error.message);
+        return res.status(500).json({message: serverErrMsg});
+    }
+}
 
+const acceptRequest = async (req, res) => {
+    try {
+        const {_id, ...data} = req.body;
+        const place = await Place.updateOne({_id}, data);
+        return res.json({
+            message: "Success to update place",
+            data: {id: place._id}
+        });
+    } catch (error) {
+        console.log(error);
+        if (error.code)
+            return res.status(error.code).json(error.message);
+        return res.status(500).json({message: serverErrMsg});
+    }
 }
 
 const rejectRequest = async (req, res) => {
-    
+    try {
+        const {_id, ...data} = req.body;
+        const place = await Place.updateOne({_id}, data);
+        // hapus ownership sementara
+        const owner = await Owner.find({placesId: {_id}})
+        await owner.delete();
+        return res.json({
+            message: "Success to update place",
+            data: {id: place._id}
+        });
+    } catch (error) {
+        console.log(error);
+        if (error.code)
+            return res.status(error.code).json(error.message);
+        return res.status(500).json({message: serverErrMsg});
+    }
 }
 
-const deleteRequest = async (req, res) => {
-    
-}
+// const getOwnerPlaceStatus = async (req, res) => {
+//     try {
+//         const id = req.params.id;
+//         let data = await Owner.find({placesId: id})
+//         return res.json({
+//             message: "Sukses", data
+//         })
+//     } catch (error) {
+//         console.log(error);
+//         if (error.code)
+//             return res.status(error.code).json(error.message);
+//         return res.status(500).json({message: serverErrMsg});
+//     }
+// }
+
+// const deletePlace = async (req, res) => {
+//     try {
+//         const place = await Place.findOne({_id: req.params.id});
+//         await place.delete();
+//         return res.json({message: "Success to delete places"})
+//     } catch (error) {
+//         console.log(error);
+//         if (error.code)
+//             return res.status(error.code).json(error.message);
+//         return res.status(500).json({message: serverErrMsg});
+//     }
+// }
+
+// const updatePlace = async (req, res) => {
+//     try {
+//         const {_id, ...data} = req.body;
+//         const place = await Place.updateOne({_id}, data);
+//         return res.json({
+//             message: "Success to update place",
+//             data: {id: place._id}
+//         });
+//     } catch (error) {
+//         console.log(error);
+//         if (error.code)
+//             return res.status(error.code).json(error.message);
+//         return res.status(500).json({message: serverErrMsg});
+//     }
+// }
 
 module.exports = {
     getRequestedPlaces,
+    getOneRequestedPlace,
     acceptRequest,
-    rejectRequest,
-    deleteRequest
+    rejectRequest
 }
--- public/js/admin/list_approval.js
+++ public/js/admin/list_approval.js
@@ -74,9 +74,44 @@ var app = new Vue({
             });
             if (res.ok) {
                 toastr.success("Success to update time")
-                this.loadPlaces();
+                this.loadApprovals();
             } else toastr.error("Failed to delete data");
 
+        },
+        acceptRequest: async function (id, contactNumber, contactType, placeName) {
+            const res = await fetch('/api/v1/approvals/accept', {
+                method: "PUT",
+                body: JSON.stringify({_id: id, is_requested: false, is_partner: true}),
+                headers: {'Content-Type': "application/json"}
+            });
+            if (res.ok) {
+                toastr.success("Success to accept the ownership")
+                this.loadApprovals();
+                this.sendWhatsapp(contactNumber, contactType, "Hallo%20kak%2C%20kami%20dari%20emam.id%20bahwa%20restoran%20anda%20yang%20bernama%20"+placeName+"%20telah%20kami%20terima%20dari%permintaan%20kakak%2E%20Terima%20kasih%20telah%20menjadi%20partner%20kami%21")
+            } else toastr.error("Failed to accept the ownership");
+
+        },
+        rejectRequest: async function (id, contactNumber, contactType, placeName) {
+            const res = await fetch('/api/v1/approvals/reject', {
+                method: "PUT",
+                body: JSON.stringify({_id: id, is_requested : false}),
+                headers: {'Content-Type': "application/json"}
+            });
+            if (res.ok) {
+                toastr.success("Success to accept the ownership")
+                this.loadApprovals();
+                this.sendWhatsapp(contactNumber, contactType, "Hallo%20kak%2C%20kami%20dari%20emam.id%20bahwa%20restoran%20anda%20yang%20bernama%20"+placeName+"%2E%20Mohon%20maaf%2C%20bahwa%20kami%20belum%20dapat%20menyetujui%20permintaan%20kakak%2E%20Kemungkinan%2C%20kami%20memerlukan%20data%20yang%20valid%20dan%20jelas%2E%20Jika%20sudah%20disiapkan%2C%20Kakak%20dapat%20mengulangi%20proses%20klaim%20tempat%20restoran%2E")
+            } else toastr.error("Failed to accept the ownership");
+
+        },
+        sendWhatsapp: function (number, type, content) {
+            if (type == "022") {
+                toastr.error("Nomor ini tidak terdaftar di whatsapp");
+            } else {
+                setTimeout(() => {
+                    location.href = "https://api.whatsapp.com/send/?phone=62" + number + "&text="+content+"&app_absent=0";
+                }, 1500)
+            }
         },
         loadApprovals: async function () {
             const res = await fetch('/api/v1/approvals');
